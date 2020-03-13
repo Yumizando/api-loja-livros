87a62ad4c79abe625d82c66b9872a9279675c152
Yumizando <laryyumi@gmail.com>
2020-03-13 10:12:40 -0300

    Projeto criado.

---
+++ src/main/java/br/com/apilojalivros/ApiLojaLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.apilojalivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLojaLivrosApplication.class, args);
+	}
+
+}
